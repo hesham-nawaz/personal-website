23182cc7b4662a3a648c7fb965b3b796a41fc2f3
Hesham Nawaz <heshamnawaz2000@gmail.com>
2025-05-03 18:06:35 -0700

    "more"

--- js/load-nav.js
+++ js/load-nav.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         <nav class="top-nav">
           <ul>
             <li><a href="index.html">home</a></li>
-            <li><a href="#">about</a></li>
+            <li><a href="about.html">about</a></li>
             <li><a href="projects.html">projects</a></li>
            </ul>
         </nav>
